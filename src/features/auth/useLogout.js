1f7297d3edefe3b5edc51a02c496ddf4a74423b8
MrLibutan <libuts08@gmail.com>
2025-09-02 20:35:21 +0800

    fix: clear cache every logout

--- src/features/auth/useLogout.js
+++ src/features/auth/useLogout.js
@@ -1,12 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { logout as logoutAPI } from "../../services/api";
 
 export function useLogout() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { isPending, mutate: logout } = useMutation({
     mutationFn: logoutAPI,
     onSuccess: () => {
+      queryClient.clear();
       navigate("/login", { replace: true });
     },
   });
